1c3edbb268776f55468275fcbd81c9b2749db25d
Alexandre Lima <alexlimatds@gmail.com>
2026-02-14 14:39:55 -0300

    Adding pause functionality

--- level01.py
+++ level01.py
@@ -2,7 +2,14 @@ import game, pygame, sys
 
 def run(player_lives):
   ## VARIABLES ##
-  in_game = True
+  IN_GAME = 0
+  PAUSED = 1
+  LOST_LIFE = 2
+  game_state = IN_GAME
+
+  ## TEXT ##
+  font_pause = pygame.font.Font(None, 24)
+  txt_paused = font_pause.render("P A U S E", True, 'red')
 
   ## SPRITES ##
   arena = game.Arena()
@@ -20,7 +27,7 @@ def run(player_lives):
         pygame.quit()
         sys.exit()
       elif event.type == pygame.KEYDOWN:
-        if in_game:
+        if game_state == IN_GAME:
           if event.key == pygame.K_LEFT:
             player.to_left()
           if event.key == pygame.K_RIGHT:
@@ -28,19 +35,32 @@ def run(player_lives):
           if event.key == pygame.K_UP:
             player.to_aura()
       elif event.type == pygame.KEYUP:
-        if in_game:
-          if (event.key == pygame.K_LEFT and player.state == player.RUNNING_LEFT) or \
-             (event.key == pygame.K_RIGHT and player.state == player.RUNNING_RIGHT) or \
-             (event.key == pygame.K_UP and player.state == player.AURA):
-            player.to_idle()
+        if (event.key == pygame.K_LEFT and player.state == player.RUNNING_LEFT) or \
+           (event.key == pygame.K_RIGHT and player.state == player.RUNNING_RIGHT) or \
+           (event.key == pygame.K_UP and player.state == player.AURA):
+          player.to_idle()
+        if game_state == IN_GAME:
+          if event.key == pygame.K_p:
+            game_state = PAUSED
+        elif game_state == PAUSED:
+          if event.key == pygame.K_p:
+            game_state = IN_GAME
 
     ### GAME LOGIC ###
-    all_sprites.update()
-    arena.check_bump(ball)
+    if game_state == IN_GAME:
+      all_sprites.update()
+      arena.check_bump(ball)
+      collided = pygame.sprite.spritecollide(ball, [player], False, pygame.sprite.collide_mask)
+      if collided:
+        pass
 
     ### RENDERING ###
     game.screen.fill((0, 0, 0))
     all_sprites.draw(game.screen)
+    if game_state == PAUSED:
+      game.screen.blit(
+        txt_paused, 
+        (game.SCREEN_WIDHT / 2 - txt_paused.get_rect().w / 2, game.SCREEN_HEIGHT / 2 - txt_paused.get_rect().h / 2))
     pygame.display.flip()
     game.clock.tick(45) # FPS
 
